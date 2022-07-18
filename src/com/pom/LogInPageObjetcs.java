6d8d9810bb3d858d3f8929bc6c08fddf68efa88b
jeevakrishan96@gmail.com <mg9159214653>
2022-07-18 21:24:52 +0530

    added @assert

---
+++ src/com/pom/LogInPageObjetcs.java
@@ -0,0 +1,47 @@
+package com.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogInPageObjetcs{
+	public WebDriver driver=null;
+	
+	public LogInPageObjetcs (WebDriver driver) {
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+
+		
+	}
+
+	
+
+	@FindBy (id="txtUsername")
+	private WebElement username;
+	
+	
+
+	public  WebElement getUsername() {
+		return username;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getLogin() {
+		return login;
+	}
+
+	@FindBy (id="txtPassword")
+	private WebElement password;
+	
+	@FindBy (id="btnLogin")
+	private WebElement login;
+
+	
+
+	
+	
+}
